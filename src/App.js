5f433e237b6f7854e3d7b66ae032a3745bb002a0
HildoBijl <hildobijl@gmail.com>
2017-08-20 13:46:20 +0200

    Implement log-in functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,96 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase.js';
+
+firebase.auth().getRedirectResult().then((result) => {
+  console.log('Stuff happened')
+  console.log(firebase.auth().currentUser)
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+    console.log('Succes! ' + token)
+  }
+  // The signed-in user info.
+  var user = result.user;
+  console.log(user)
+  console.log(user.displayName)
+  console.log(user.email)
+  console.log(user.uid)
+}).catch((error) => {
+  console.log('Error happened')
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(errorCode +':'+errorMessage+':' + email + ':' + credential)
+  console.log(credential)
+  // ...
+});
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      user: null
+    }
+  }
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(() => {
+      console.log('Now we are ready')
+      const user = firebase.auth().currentUser
+      if (user) {
+        console.log('Logged in!')
+        this.setState({
+          user: {
+            name: user.displayName,
+            email: user.email,
+            uid: user.uid,
+          }
+        })
+      } else {
+        console.log('Logged out!')
+        this.setState({
+          user: null
+        })
+      }
+    });
+  }
+  signInGoogle() {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider)
+      .then((user) => {
+        console.log('Log in success G')
+        console.log(user)
+      })
+      .catch((err) => {
+        console.log('Log in failed G')
+        console.log(err)
+      })
+  }
+  signInFacebook() {
+    var provider = new firebase.auth.FacebookAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
+      .then((user) => {
+        console.log('Log in success F')
+        console.log(user)
+      })
+      .catch((err) => {
+        console.log('Log in failed F')
+        console.log(err)
+      })
+  }
+  // async signOut() {
+  //   await firebase.auth().signOut()
+  //   this.setState({user: null})
+  // }
+  signOut() {
+    firebase.auth().signOut()
+  }
   render() {
     return (
       <div className="App">
@@ -11,8 +99,22 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.user ? 'You are logged in as ' + this.state.user.name + '.' : 'You are not logged in now.'}
         </p>
+        {this.state.user ? (
+          <p onClick={this.signOut}>
+            Log me out!
+          </p>
+        ) : (
+          <div>
+            <p onClick={this.signInGoogle}>
+              Log in with Google
+            </p>
+            <p onClick={this.signInFacebook}>
+              Log in with Facebook
+            </p>
+          </div>
+        )}
       </div>
     );
   }
